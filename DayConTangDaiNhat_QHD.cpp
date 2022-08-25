ce592341ff3c6bd2f0702f71464f6c6df9d712b2
giabaophudinhthcs <111351284+giabaophudinhthcs@users.noreply.github.com>
2022-08-25 15:13:28 +0700

    Update DayConTangDaiNhat_QHD.cpp

--- DayConTangDaiNhat_QHD.cpp
+++ DayConTangDaiNhat_QHD.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[1005];
+int dp[1005], trace[1005], point;
+vector<int> traceRes;
 
 int lis(int a[], int n){
     dp[0] = 0;
@@ -14,12 +15,27 @@ int lis(int a[], int n){
             }
         }
         dp[i] = dp[jMax] + 1;
+        trace[i] = jMax;
     }
     int res = dp[1];
+    point = 1;
     for (int i = 2; i <= n; ++i){
-        if (dp[i] > res) res = dp[i];
+        if (dp[i] > res){
+            res = dp[i];
+            point = i;
+        }
     }
     return res;
+}
+
+void traceArr(int arr[]){
+    int A = point;
+    while (A != 0){
+        traceRes.push_back(arr[A]);
+        A = trace[A];
+    }
+    reverse(traceRes.begin(), traceRes.end());
+    for (auto x: traceRes) cout << x << " ";
 }
 
 int main(){
@@ -27,5 +43,6 @@ int main(){
     cin >> n;
     int a[n + 1];
     for (int i = 1; i <= n; ++i) cin >> a[i];
-    cout << lis(a, n);
+    cout << lis(a, n) << "\n";
+    traceArr(a);
 }

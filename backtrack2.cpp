2b6fc25c7cf0c9843039e74633a429e9cdb0d524
giabaophudinhthcs <111351284+giabaophudinhthcs@users.noreply.github.com>
2022-10-02 16:15:03 +0700

    Update backtrack2.cpp

--- backtrack2.cpp
+++ backtrack2.cpp
@@ -7,20 +7,20 @@ bool use[22];
 
 void printPer(){
     for (int i = 1; i <= n; ++i){
-        if (use[i]) cout << i < " ";
+        if (use[i]) cout << i << " ";
     }
     cout << "\n";
 }
 
 void back(int i){
-    for (int j = 1; j <= n; ++j){
-        used[j] = false;
-        if (i == n) printPer();
-        else back(i + 1);
-        used[j] = true;
-        if (i == n) printPer();
-        else back(i + 1);
+    if (i == n + 1){
+        printPer();
+        return;
     }
+    use[i] = false;
+    back(i + 1);
+    use[i] = true;
+    back(i + 1);
 }
 
 int main(){

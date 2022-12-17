ec1184022fc07214045ecc27a21f908e21944e19
giabaophudinhthcs <111351284+giabaophudinhthcs@users.noreply.github.com>
2022-12-17 09:44:49 +0700

    Add files via upload

---
+++ Suffix_array_1.cpp
@@ -0,0 +1,41 @@
+// Suffix array
+
+#include <bits/stdc++.h>
+
+#define len length
+#define st first
+#define nd second
+
+using namespace std;
+
+int main(){
+	string s;
+	int n;
+	cin >> s;
+	s += '$';
+	n = int(s.len());
+	vector<int> p(n), c(n);
+	vector<pair<char, int>> a(n);
+	for (int i = 0; i < n; ++i) a[i] = {s[i], i};
+	sort(a.begin(), a.end());
+	for (int i = 0; i < n; ++i) p[i] = a[i].nd;
+	c[p[0]] = 0;
+	for (int i = 1; i < n; ++i){
+		if (a[i].st == a[i - 1].st) c[p[i]] = c[p[i - 1]];
+		else c[p[i]] = c[p[i - 1]] + 1;
+	}
+	int k = 0;
+	while ((1 << k) < n){
+		vector<pair<pair<int, int>, int>> b(n);
+		for (int i = 0; i < n; ++i) b[i] = {{c[i], c[i + (1 << k) % n]}, i};
+		sort(b.begin(), b.end());
+		for (int i = 0; i < n; ++i) p[i] = b[i].nd;
+		c[p[0]] = 0;
+		for (int i = 1; i < n; ++i){
+			if (b[i].st == b[i - 1].st) c[p[i]] = c[p[i - 1]];
+			else c[p[i]] = c[p[i - 1]] + 1;
+		}
+		k++;
+	}
+	for (int i = 0; i < n; ++i) cout << p[i] << " ";
+}
